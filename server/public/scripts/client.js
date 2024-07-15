235a0ed940ad29e033110acb12c762572cae4b6c
Rohit Repu <rrepu4@gmail.com>
2024-07-15 13:31:36 -0500

    latest version

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,12 +3,8 @@ console.log('hello world');
 //let currentIndex = 3;
 let highTemp =40;
 
-/**
- * 
- */ 
-//Create  Global array
 
-let temperatureDat = [];
+let sumData = 0;
 
 // Submit form function
 function submitForm(event) {
@@ -20,49 +16,49 @@ function submitForm(event) {
 
     // Find the input field and assign the value to our variable
     
-   
     //! .value always returns a string, for a number use Number(string)
     let varFirstName = document.querySelector('#FirstName-input').value;
     let varLastName = document.querySelector('#LastName-input').value;
     let varID = document.querySelector('#ID-input').value;
-    // Find the tbody on the page so that we can append to it
     let varTitle = document.querySelector('#Title-input').value;
     let varAnnualSalary = document.querySelector('#AnnualSalary-input').value;
+//console.log(varAnnualSalary.toLocaleString());   
+//sumData = parseInt(varAnnualSalary, 10); 
+sumData += parseInt(varAnnualSalary, 10);
+document.querySelector('#Total-Monthly').innerHTML = sumData;
 
-    let DataTable = document.querySelector('#Title-input');
-    
-  
+const formatter = new Intl.NumberFormat('en-US');
+const formattedNumber = formatter.format(varAnnualSalary);
+console.log(formattedNumber);
+console.log(sumData);
 
-    // let deletebtn = document.querySelector('.delete-btn');
-    // let lastIndex = temperatureTable.lastElementChild;
-    // let tableData = Number(lastIndex.firstElementChild.innerHTML);
+
+    //Find the tbody on the page so that we can append to it
+    let DataTable = document.querySelector('#EmployeeData');
+ 
     DataTable.innerHTML += `
         <tr>
             <td>${varFirstName}</td>
             <td>${varLastName}</td>
             <td>${varID}</td>
             <td>${varTitle}</td>
-            <td>${varAnnualSalary}</td>
+            <td>&#36;${formattedNumber}</td>
            <td><button class="delete-btn" onclick="deleteTableRow()">Delete</button></td> 
            
         </tr>
     `;
 
-   
-    //currentIndex += 1;
-
-    // Check the temperature against the high temp
-    if (Number(tempVal) > highTemp) {
-        // Number(tempVal) turns a string into a number
-        highTemp = Number(tempVal);
-        document.querySelector('#high-temp').innerHTML = highTemp;
-    }
 }
+
+console.log(sumData);
 function deleteTableRow(){
+
     console.log("Is delete btn working?");
     let tableBody = document.querySelector('#EmployeeData');
     // console.log(tableBody);
     tableBody.firstElementChild.remove()
 
 }
+
+
  
